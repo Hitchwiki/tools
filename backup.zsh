a22538079d0cfca1bb4408361ad84f445c34fa7e
Philipp Gruber <philipp.gruber@flupps.net>
2013-08-25 07:04:28 +0200

    add scp, bzip2

--- backup.zsh
+++ backup.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-source db.conf
+source config 
 
 IFS=$'\n' languages=($(echo "SHOW DATABASES;" | mysql -u$DBUSERNAME -p$DBPASSWORD | grep -E '^hitchwiki_(..)$' | sed 's/^hitchwiki_//g'))
 
@@ -8,11 +8,15 @@ mkdir -p backups
 
 for l ($languages) {
 #	echo "Processing $l..."
-	mysqldump -u$DBUSERNAME -p$DBPASSWORD --ignore-table=hitchwiki_$l.objectcache hitchwiki_$l > backups/hitchwiki_$l.sql
-	size=$(ls -s backups/hitchwiki_$l.sql | cut -d' ' -f1)
-	if [[ '1000' -gt "$size" ]] {
+	mysqldump -u$DBUSERNAME -p$DBPASSWORD --ignore-table=hitchwiki_$l.objectcache hitchwiki_$l | bzip2 > backups/hitchwiki_$l.sql.bz2
+	size=$(ls -s backups/hitchwiki_$l.sql.bz2 | cut -d' ' -f1)
+	if [[ '300' -gt "$size" ]] {
 		echo "Something is wrong with backup for hitchwiki_$l. The file is very small."
 	}
+}
+
+for s ($SERVERS) {
+	scp backups/* $s:backup/current/db
 }
 
 
