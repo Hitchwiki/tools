98fb4057bd45f8f109f49fe5153042a51b5a1558
Philipp Gruber <philipp.gruber@flupps.net>
2013-08-26 13:54:16 +0200

    bugfix

--- dumpsql.zsh
+++ dumpsql.zsh
@@ -14,7 +14,7 @@ mkdir -p $BACKUP_DIR
 for db ($databases) {
 	mysqldump -u$DBUSERNAME -p$DBPASSWORD --ignore-table=$db.objectcache $db | bzip2 > $BACKUP_DIR/$db.sql.bz2
 	size=$(ls -s $BACKUP_DIR/$db.sql.bz2 | cut -d' ' -f1)
-	if [[ '300' -gt "$size" ]] {
-		err("Something might be wrong with the backup for database $db. The file is very small.")
+	if [[ '50' -gt "$size" ]] {
+		err "Something might be wrong with the backup for database $db. The file is very small."
 	}
 }
