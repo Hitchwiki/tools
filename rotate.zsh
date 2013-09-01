1fc1682c46f3cfc70eeeec5b361d47c0766c1338
Philipp Gruber <philipp.gruber@flupps.net>
2013-09-02 09:27:47 +1000

    bugfix

--- rotate.zsh
+++ rotate.zsh
@@ -7,7 +7,7 @@ cd $BACKUP_DIR
 week=`date "+%V"`
 old=`date "+%V" --date="-6 weeks"`
 
-outdated=`find -type f -mtime +3 current`
+outdated=`find current -type f -mtime +3`
 
 if [[ -n $outdated ]] {
 	err "Warning: the following files are older than 3 days:\n$outdated"
