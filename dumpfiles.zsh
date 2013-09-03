15957d7e46838d11354f8d14fa5cc00301420ba7
Philipp Gruber <philipp.gruber@flupps.net>
2013-09-03 06:10:05 +0200

    dump files as well

--- dumpfiles.zsh
+++ dumpfiles.zsh
@@ -1,3 +1,18 @@
 #!/bin/zsh
 
 source lib.zsh
+
+mkdir -p $BACKUP_DIR/files/
+
+for directory ($DIRS) {
+	for dir ($directory*) {
+		if [[ -d $dir && ! -h $dir ]] {
+			plain=`basename $dir`
+			tar cfj $BACKUP_DIR/files/$plain.tar.bz2 $dir 
+		}
+		size=$(ls -s $BACKUP_DIR/files/$plain.tar.bz2 | cut -d' ' -f1)
+		if [[ '10' -gt "$size" ]] {
+			err "Something might be wrong with the backup file $plain.tar.gz. The file is very small."
+		}
+	}
+}
