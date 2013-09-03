a4779b68e08c777f2837e0eda4386b8233cc2394
Philipp Gruber <philipp.gruber@flupps.net>
2013-09-03 05:25:27 +0200

    minor tweaking

--- dumpsql.zsh
+++ dumpsql.zsh
@@ -12,9 +12,9 @@ if [[ -z $databases ]] {
 mkdir -p $BACKUP_DIR
 
 for db ($databases) {
-	mysqldump -u$DBUSERNAME -p$DBPASSWORD --ignore-table=$db.objectcache $db | bzip2 > $BACKUP_DIR/$db.sql.bz2
+	mysqldump -u$DBUSERNAME -p$DBPASSWORD --ignore-table=$db.objectcache --ignore-table=$db.l10n_cache $db | bzip2 > $BACKUP_DIR/$db.sql.bz2
 	size=$(ls -s $BACKUP_DIR/$db.sql.bz2 | cut -d' ' -f1)
-	if [[ '50' -gt "$size" ]] {
+	if [[ '30' -gt "$size" ]] {
 		err "Something might be wrong with the backup for database $db. The file is very small."
 	}
 }
