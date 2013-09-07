94644706033c93230f922c2b8d44b17d7e8fcd19
Philipp Gruber <philipp.gruber@flupps.net>
2013-09-07 06:01:55 +0200

    fine tuning

--- dumpsql.zsh
+++ dumpsql.zsh
@@ -2,7 +2,7 @@
 
 source lib.zsh
 
-IFS=$'\n' databases=($(echo "SHOW DATABASES;" | mysql -u$DBUSERNAME -p$DBPASSWORD | grep -E '^[a-z]'))
+IFS=$'\n' databases=($(echo "SHOW DATABASES;" | mysql -u$DBUSERNAME -p$DBPASSWORD | grep -Ev $EXCLUDE_DB))
 
 if [[ -z $databases ]] {
 	err "No databases found."
@@ -14,7 +14,7 @@ mkdir -p $BACKUP_DIR/db
 for db ($databases) {
 	mysqldump -u$DBUSERNAME -p$DBPASSWORD --ignore-table=$db.objectcache --ignore-table=$db.l10n_cache $db | bzip2 > $BACKUP_DIR/db/$db.sql.bz2
 	size=$(ls -s $BACKUP_DIR/db/$db.sql.bz2 | cut -d' ' -f1)
-	if [[ '10' -gt "$size" ]] {
+	if [[ '4' -gt "$size" ]] {
 		err "Something might be wrong with the backup for database $db. The file is very small."
 	}
 }
