e9471bd6f032b1985ef580763a00794ce1dfc040
Philipp Gruber <philipp.gruber@flupps.net>
2013-10-01 04:27:17 +0200

    don't warn when files are too small

--- dumpfiles.zsh
+++ dumpfiles.zsh
@@ -8,11 +8,8 @@ for directory ($DIRS) {
 	for dir ($directory*) {
 		if [[ -d $dir && ! -h $dir ]] {
 			plain=`basename $dir`
-			tar cfj $BACKUP_DIR/files/$plain.tar.bz2 $dir 
-		}
-		size=$(ls -s $BACKUP_DIR/files/$plain.tar.bz2 | cut -d' ' -f1)
-		if [[ '10' -gt "$size" ]] {
-			err "Something might be wrong with the backup file $plain.tar.gz. The file is very small."
+			tar cfjP $BACKUP_DIR/files/$plain.tar.bz2 $dir 
+			size=$(ls -s $BACKUP_DIR/files/$plain.tar.bz2 | cut -d' ' -f1)
 		}
 	}
 }
