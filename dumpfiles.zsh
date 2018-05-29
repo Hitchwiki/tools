fd0812b378d8247f7790a7c7c7f0bd64b580cbe6
Philipp Gruber <philipp.gruber@flupps.net>
2018-05-30 01:48:49 +0200

    exclude dir

--- dumpfiles.zsh
+++ dumpfiles.zsh
@@ -8,7 +8,7 @@ for directory ($DIRS) {
 	for dir ($directory*) {
 		if [[ -d $dir && ! -h $dir ]] {
 			plain=`basename $dir`
-			tar cfjP $BACKUP_DIR/files/$plain.tar.bz2 $dir 
+			tar cfjP $BACKUP_DIR/files/$plain.tar.bz2 --exclude=$EXCLUDE_DIR $dir
 			size=$(ls -s $BACKUP_DIR/files/$plain.tar.bz2 | cut -d' ' -f1)
 		}
 	}
